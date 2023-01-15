2e77ce2dbfb5ed15424dad35982bd1683194522b
Islam <islomjonalimov83@gmail.com>
2023-01-15 05:12:43 +0000

    changes

--- Counter/app.js
+++ Counter/app.js
@@ -1,16 +1,3 @@
-function Counter(element, value) {
-    this.counter = element;
-    this.value = value;
-    this.reset = element.querySelector(".reset");
-    this.increaseBtn = element.querySelector(".increase");
-    this.decreaseBtn = element.querySelector(".decrease");
-    this.valueDOM = element.querySelector(".value");
-    this.valueDOM.textContent = this.value;
-}
-
-const firtsCounter = new Counter(getElement(".first-counter"), 0);
-const secondCounter = new Counter(getElement(".second-counter"), 0);
-
 function getElement(selection) {
     const element = document.querySelector(selection);
     if (element) {
@@ -20,3 +7,35 @@ function getElement(selection) {
         `Please check "${selection}" selector, no such element exists`
     );
 }
+
+function Counter(element, value) {
+    this.counter = element;
+    this.value = value;
+    this.reset = element.querySelector(".reset");
+    this.increaseBtn = element.querySelector(".increase");
+    this.decreaseBtn = element.querySelector(".decrease");
+    this.valueDOM = element.querySelector(".value");
+    this.valueDOM.textContent = this.value;
+}
+// Increase
+Counter.prototype.increase = function() {
+    console.log(this);
+    this.value++;
+    this.valueDOM.textContent = this.value;
+};
+// Decrease
+Counter.prototype.decrease = function() {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+};
+// Reset
+Counter.prototype.reset = function() {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+};
+
+const firtsCounter = new Counter(getElement(".first-counter"), 0);
+const secondCounter = new Counter(getElement(".second-counter"), 0);
+
+firtsCounter.increase();
+secondCounter.increase();
