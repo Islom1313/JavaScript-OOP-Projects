03bd822e55705899495defa8902085175e3721b9
Islam <islomjonalimov83@gmail.com>
2023-01-15 04:50:25 +0000

    new changes

--- Counter/app.js
+++ Counter/app.js
@@ -1,1 +1,16 @@
-const btn = document.querySelector("");
+function Counter(element, value) {
+    console.log(element, value);
+}
+
+const firtsCounter = new Counter(getElement(".first-counter"), 1);
+const secondCounter = new Counter(getElement(".second-counter"), 20);
+
+function getElement(selection) {
+    const element = document.querySelector(selection);
+    if (element) {
+        return element;
+    }
+    throw new Error(
+        `Please check "${selection}" selector, no such element exists`
+    );
+}
