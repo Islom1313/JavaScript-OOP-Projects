036baf0e62e9e89e75bb2415b675749c0f64ea30
Islam <islomjonalimov83@gmail.com>
2023-01-16 06:18:44 +0000

    changed

--- Gallery/app.js
+++ Gallery/app.js
@@ -9,7 +9,9 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+    this.container = element;
     this.list = [...element.querySelectorAll(".img")];
+    // target
     this.modal = getElement(".modal");
     this.mainImg = getElement(".main-img");
     this.modalImages = getElement(".modal-images");
@@ -17,7 +19,22 @@ function Gallery(element) {
     this.closeBtn = getElement(".close-btn");
     this.nextBtn = getElement(".next-btn");
     this.prevBtn = getElement(".prev-btn");
+    // self ref
+    let self = this;
+    // bind funtions
+    // this.openModal = this.openModal.bind(this);
+    this.container.addEventListener(
+        "click",
+        function(e) {
+            // self.openModal();
+            this.openModal();
+        }.bind(this)
+    );
 }
+Gallery.prototype.openModal = function(e) {
+    // console.log(this);
+    this.modal.classList.add("open");
+};
 
 const nature = new Gallery(getElement(".nature"));
 const city = new Gallery(getElement(".city"));
