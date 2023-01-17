3e88b9a7366cba8a3b48284e8f918458bc89e44a
Islam <islomjonalimov83@gmail.com>
2023-01-17 18:10:12 +0000

    new changes

--- Gallery/app.js
+++ Gallery/app.js
@@ -36,7 +36,11 @@ function Gallery(element) {
 }
 Gallery.prototype.openModal = function(selectedImage, list) {
     this.setMainImage(selectedImage);
-    this.modalImages.innerHtml = list.map(function(image) {});
+    this.modalImages.innerHtml = list.map(function(image) {
+        return `<img src= "${
+      image.src
+    }" title="${image.title}" data-id="${image.data.id}" class="${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}" >`;
+    });
     this.modal.classList.add("open");
 };
 
